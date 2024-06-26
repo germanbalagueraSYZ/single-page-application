7e96238e7e378d8124cf1d21d0eda5f742742b7f
germanbalagueraSYZ <german.balaguera@grupo-syz.com>
2024-06-26 12:26:40 -0500

    useEfect, axios Linkto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import './assets/css/componentes/card.css';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Page404 from './pages/Page404';
-import Header from './components/Header';
+import Header from './components/header';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
